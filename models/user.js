557c0d68295524d57cd1e5faf29853738f446f93
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-06 18:01:08 -0500

    installs and configures bcrypt

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_ROUNDS = 6;
 
 
 const userSchema = new Schema({
@@ -13,5 +15,15 @@ const userSchema = new Schema({
     password: String
 }, { timestamps: true });
 
+
+userSchema.pre('save', function(next) {
+    const user = this;
+    if(!user.isModified("password")) return next();
+    bcrypt.hash(user.password, SALT_ROUNDS, function(err, hash) {
+        if(err) return next(err);
+        user.password = hash;
+        next();
+    })
+});
 
 module.exports = mongoose.model('User', userSchema);
