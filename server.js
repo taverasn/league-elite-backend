a28ac80462649ff0dd3e8f77f5681e63721471ca
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-06 18:14:27 -0500

    adds working server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const logger = require('morgan');
+
+
+require('dotenv').config();
+require('./config/database');
+
+const app = express();
+
+// mount our middleware
+app.use(express.json());
+app.use(logger('dev'));
+// app.use(cors());
+
+
+// mount our routes
+// app.use('/api/users', require('./routes/api/users'));
+
+const port = process.env.PORT || 3001
+// tell express to listen
+app.listen(port, function() {
+    console.log(`Express is listening in port ${port}`);
+});
