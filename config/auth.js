8087c128464bebf81ddb3d78b097f8ad9a805f16
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-16 11:27:03 -0500

    Setsup Authentication for connection to the backend

---
+++ config/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
+
+module.exports = function(req, res, next) {
+    let token = req.get('Authorization');
+    if(token) {
+        token = token.replace('Bearer ', '');
+        jwt.verify(token, SECRET, function(err, decoded) {
+            if(err) next(err);
+            req.user = decoded.user;
+            next();
+        });
+    } else {
+        next();
+    }
+}
