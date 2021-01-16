1a993b7f6175c38b6adfd7c41210411ca3af5fef
Nicholas Taveras III <GAStudent@Nicholass-iMac.local>
2021-01-16 11:28:20 -0500

    Adds functions to allow the user to login, signup and have a token created

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,47 @@
 const User = require('../models/user');
-
+const SECRET = process.env.SECRET;
+const jwt = require('jsonwebtoken');
 
 module.exports = {
-    signup
+    signup,
+    login
 }
 
 
 async function signup(req, res) {
     try {
         const user = await User.create(req.body);
-        res.json({ user })
+
+        const token = createJWT(user);
+
+        res.json({ token })
+
     } catch (error) {
+        
         res.status(400).json({ msg: 'bad request' });
     }
+}
+
+async function login(req, res) {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+        if(!user) return res.status(401).json({ err: 'bad credentials' });
+        
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(isMatch) {
+                const token = createJWT(user);
+                res.json({ token });
+            } else {
+                res.status(400).json({ err: 'bad request' });
+            }
+        });
+
+    } catch (error) {
+
+    }
+}
+
+// helper function for generating JWTs
+function createJWT(user) {
+    return jwt.sign({ user }, SECRET, { expiresIn: '24h' })
 }
