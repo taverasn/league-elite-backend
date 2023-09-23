78dbeffd185da10a3123bb2baf09fc4171dc9c8a
Nicholas Taveras <taveras123n@gmail.com>
2023-09-23 01:40:45 -0400

    Added use statement for cors middleware

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
 
-require('dotenv').config();
-require('./config/database');
+require("dotenv").config();
+require("./config/database");
 
 const app = express();
 
 // mount our middleware
 app.use(express.json());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 
-
 // mount our routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/guides', require('./routes/api/guides'));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/guides", require("./routes/api/guides"));
+app.use(cors({ origin: "https://league-elite.netlify.app" }));
 
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3001;
 // tell express to listen
-app.listen(port, function() {
-    console.log(`Express is listening in port ${port}`);
+app.listen(port, function () {
+  console.log(`Express is listening in port ${port}`);
 });
